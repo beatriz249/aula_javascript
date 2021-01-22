1fbd50d9d9456cbb6d2f45bee00805204703e5e3
beatriz249 <bia_249@yahoo.com>
2021-01-21 22:04:40 -0300

    Primeiro commit, aula 1 Programação para internet com JavaScript na DIO

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var nome = "Beatriz Alves";
+var idade = "22";
+var idade2 = "10";
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade+idade2);
+console.log(nome);
+console.log(idade * idade2);
+console.log(frase.toLowerCase());
+//console.log(frase.toUpperCase());
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
